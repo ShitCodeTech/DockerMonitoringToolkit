5f0d7bd9ddcecf545f488c28e6243588ce54423f
LW <sdjfklsdfklsdjl@mail.ru>
2024-11-26 00:21:52 +0500

    ghjgjhgjh

--- autobuild.sh
+++ autobuild.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# repo_url="https://github.com/ShitCodeTech/DockerMonitoringToolkit"
-# repo_dir="DockerMonitoringToolkit"
-
+clone_comand="git clone -b serverside https://github.com/ShitCodeTech/DockerMonitoringToolkit.git"
+repo_dir="DockerMonitoringToolkit"
 build_command="`docker build --build-arg ID="$ID" --build-arg PRETTY_NAME="$PRETTY_NAME" -t server_monitor .`"
+start_command="docker run -p 8080:8080 -d server_monitor"
 
-start_command="docker run -p 8080:8080 server_monitor"
-
-banner=$(cat << 'EOF'
+banner=$tput setaf 3;(cat << 'EOF'
   █████████ █████     ███ █████     █████████             █████       ███████████           █████     
  ███░░░░░██░░███     ░░░ ░░███     ███░░░░░███           ░░███       ░█░░░███░░░█          ░░███      
 ░███    ░░░ ░███████ ███████████  ███     ░░░  ██████  ███████  █████░   ░███  ██████ ██████░███████  
@@ -17,7 +15,7 @@ banner=$(cat << 'EOF'
 ░░█████████ ████ █████████░░█████ ░░█████████░░██████░░███████░░██████   ████░░█████░░██████████ █████
  ░░░░░░░░░ ░░░░ ░░░░░░░░░  ░░░░░   ░░░░░░░░░  ░░░░░░  ░░░░░░░░ ░░░░░░   ░░░░░ ░░░░░░ ░░░░░░░░░░ ░░░░░ 
 EOF
-)
+);tput setaf default
 
 cols=$(tput cols)
 while IFS= read -r line; do
@@ -27,16 +25,17 @@ while IFS= read -r line; do
         padding=$(( (cols - ${#line}) / 2 ))
         printf "%${padding}s%s\n" "" "$line"
     fi
+tput setaf $1
 done <<< "$banner"
+tput setaf default
 
 echo "Starting the installation process..."
 
-# echo "Cloning repository from GitHub..."
-# git clone "$repo_url"
-# cd "$repo_dir" || { echo "Failed to enter directory $repo_dir"; exit 1; }
+echo "Cloning repository from GitHub..."
+$clone_comand
+cd "$repo_dir" || { echo "Failed to enter directory $repo_dir"; exit 1; }
 
 echo "Initializing build process..."
-
 bar_length=40
 total_steps=100
 
@@ -52,23 +51,28 @@ for i in $(seq 1 $total_steps); do
     echo -ne "\r$progress $percentage%"
     sleep 0.01
 done
-
 echo
 
 if [[ -f /etc/os-release ]]; then
     HOST_ID=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 |  tr -d '"')
     HOST_PRETTY_NAME=$(grep "^PRETTY_NAME=" /etc/os-release | cut -d'=' -f2 |  tr -d '"')
-    export ID=$HOST_ID
-    export PRETTY_NAME=$HOST_PRETTY_NAME
+    ID=$HOST_ID
+    PRETTY_NAME=$HOST_PRETTY_NAME
 else
     echo "/etc/os-release not found. Exiting."
-    exit 1
+    ID=''
+    PRETTY_NAME=''
 fi
 
-echo "Running build command: $build_command"
+echo "Building container..."
 $build_command
 
-echo "Starting start command: $sdjfkd"
+echo "Starting container..."
 $start_command
+
+cd ..
+rm -rf $repo_dir
+
+docker ps
 
 echo "Installation completed successfully!"
